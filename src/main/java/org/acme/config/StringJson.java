4b52b58f4eeb44b6e18e336abc181511e1912696
Mamadou Yaya DIALLO <myaya.diallo@wuriguinee.com>
2020-10-18 18:39:02 +0000

    Initial commit

---
+++ src/main/java/org/acme/config/StringJson.java
@@ -0,0 +1,20 @@
+package org.acme.config;
+
+public class StringJson {
+    public String name;
+
+    public StringJson() {
+    }
+
+    public StringJson(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
